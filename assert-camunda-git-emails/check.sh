2bbc16a9279fe6c732072ef60ee09c48d136244e
Christian Nicolai <christian.nicolai@camunda.com>
2022-01-12 09:16:56 +0100

    chore(action): allow specifying additional allowed email grep patterns

--- assert-camunda-git-emails/check.sh
+++ assert-camunda-git-emails/check.sh
@@ -2,6 +2,15 @@
 
 set -eu
 
+# This variable has to contain regex expression for the grep tool
+HARDCODED_ALLOWED_EMAILS_REGEX="@camunda.com\|@users.noreply.github.com\|noreply@github.com"
+
+if [ "$ADDITIONAL_ALLOWED_EMAILS_REGEX" = "" ]; then
+  ALLOWED_EMAILS_REGEX="$HARDCODED_ALLOWED_EMAILS_REGEX"
+else
+  ALLOWED_EMAILS_REGEX="$HARDCODED_ALLOWED_EMAILS_REGEX\|$ADDITIONAL_ALLOWED_EMAILS_REGEX"
+fi
+
 echo "Looking at Git commits in range $GIT_RANGE"
 
 echo "All found email addresses:"
@@ -9,16 +18,18 @@ git log --pretty=tformat:"%ae%n%ce" "$GIT_RANGE" | sort | uniq
 
 # shellcheck disable=SC2126
 NUM_VIOLATIONS=$(git log --pretty=tformat:"%ae%n%ce" "$GIT_RANGE" | \
-  grep -v "@camunda.com\|@users.noreply.github.com\|noreply@github.com" | \
+  grep -v "$ALLOWED_EMAILS_REGEX" | \
   wc -l)
 
-if [ "$NUM_VIOLATIONS" -ne 0 ]; then
+if [ "$NUM_VIOLATIONS" -eq 0 ]; then
+  echo "Success! No violating email addresses found."
+
+  exit 0
+else
   echo "Failure! Found $NUM_VIOLATIONS violating email addresses:"
   git log --pretty=tformat:"%ae%n%ce" "$GIT_RANGE" | \
-    grep -v "@camunda.com\|@users.noreply.github.com\|noreply@github.com" | \
+    grep -v "$ALLOWED_EMAILS_REGEX" | \
     sort | uniq
 
   exit 1
 fi
-
-exit 0
