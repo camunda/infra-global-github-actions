1c19f2469fa4ccd1fe19c033c63157758638bf38
Clément Nero <clement.nero@camunda.com>
2024-10-23 18:06:17 +0200

    chore(rerun-failed-run): check for errors in failure annotations

---
+++ rerun-failed-run/check-annotations-and-logs/find-errors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -euo pipefail
+
+# Global variables
+ERROR_MESSAGES=${ERROR_MESSAGES?}
+GH_REPO=${GH_REPO?}
+GH_TOKEN=${GH_TOKEN?}
+RELATIVE_SCRIPT_PATH=$(dirname "$0")
+RUN_ID=${RUN_ID?}
+
+# Fetch failed logs of the workflow run, i.e. with an exit status != 0
+
+logs=$(
+  gh run view "$RUN_ID" \
+    --exit-status --log-failed || true
+)
+
+# Fetch annotations of the workflow run (related jobs)
+# Id of the WorkflowRun object is needed to fetch annotations from GitHub GraphQL API
+node_id=$(
+  gh api \
+    "/repos/{owner}/{repo}/actions/runs/${RUN_ID}" \
+    --jq '.node_id'
+)
+annotations=$(
+  gh api graphql \
+    --paginate \
+    --field nodeId="${node_id}" \
+    --field query="@$RELATIVE_SCRIPT_PATH/graphql/query_get_annotations_by_workflow_run.graphql" \
+    --jq '.data.node.checkSuite.checkRuns.edges[].node.annotations.edges[] | select(.node.annotationLevel == "FAILURE").node.message'
+)
+
+# Check for error messages in logs and annotations
+found=$(
+  echo "$logs" "$annotations" | \
+  jq -c -n -R \
+    --argjson errors "${ERROR_MESSAGES}" '[inputs] | any(test($errors | join("|")))'
+)
+
+echo "${found}"
