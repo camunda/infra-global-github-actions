b5c71f6193fb721f7202fa41cd7a9a1ad357eb94
Luca Cherubin <Kerruba@users.noreply.github.com>
2024-11-27 16:06:07 +0100

    chore: adds a way to save error logs to file for cusomption from composite action

--- actionlint/sanitize-config.sh
+++ actionlint/sanitize-config.sh
@@ -3,19 +3,24 @@ set -e
 
 CUSTOM_CONFIG_FILE="$1"
 DEFAULT_CONFIG_FILE="$2"
+ERROR_OUTPUT_FILE="${3:-/tmp/actionlint-sanitize-error.log}"
+
+error() {
+  echo "$@" >>"$ERROR_OUTPUT_FILE"
+}
 
 yq -r '.self-hosted-runner.labels' "$CUSTOM_CONFIG_FILE" | sort -u >/tmp/custom_runner_labels
 yq -r '.self-hosted-runner.labels' "$DEFAULT_CONFIG_FILE" | sort -u >/tmp/supported_runner_labels
 UNSUPPORTED_RUNNERS=$(comm -13 /tmp/supported_runner_labels /tmp/custom_runner_labels) # extract invalid labels from custom_runners
 if [ -n "$UNSUPPORTED_RUNNERS" ]; then
   # Print warning messages on stderr
-  >&2 echo "These self-hosted labels are not supported and will be removed from actionlint configuration"
-  >&2 echo "$UNSUPPORTED_RUNNERS"
+  error "These self-hosted labels are not supported and will be removed from actionlint configuration"
+  error "$UNSUPPORTED_RUNNERS"
 fi
 VALID_RUNNERS="$(comm -12 /tmp/supported_runner_labels /tmp/custom_runner_labels)"
 if [ -z "$VALID_RUNNERS" ]; then
-  >&2 echo "No valid self-hosted labels found in your actionlint configuration"
-  >&2 echo "Fallback to default runners"
+  error "No valid self-hosted labels found in your actionlint configuration"
+  error "Fallback to default runners"
   VALID_RUNNERS=$(cat /tmp/supported_runner_labels)
 fi
 valid_runners="$VALID_RUNNERS" yq '.self-hosted-runner.labels = env(valid_runners)' "$CUSTOM_CONFIG_FILE"
