1024cd9009ec656031858e38e5158d717845315b
Luca Cherubin <Kerruba@users.noreply.github.com>
2024-12-03 18:16:56 +0100

    chore: update santize script to use bash and proper error flags

--- actionlint/sanitize-config.sh
+++ actionlint/sanitize-config.sh
@@ -1,13 +1,10 @@
-#!/usr/bin/env sh
-set -e
+#!/usr/bin/env bash
+set -euo
 
 CUSTOM_CONFIG_FILE="$1"
 DEFAULT_CONFIG_FILE="$2"
-# TODO: Implement error logging
-# ERROR_OUTPUT_FILE="${3:-/tmp/actionlint-sanitize-error.log}"
 
-error() {
-  # echo "$@" >>"$ERROR_OUTPUT_FILE"
+warn() {
   >&2 echo "$@"
 }
 
@@ -16,15 +13,15 @@ yq -r '.self-hosted-runner.labels' "$DEFAULT_CONFIG_FILE" | sort -u >/tmp/suppor
 UNSUPPORTED_RUNNERS=$(comm -13 /tmp/supported_runner_labels /tmp/custom_runner_labels) # extract invalid labels from custom_runners
 if [ -n "$UNSUPPORTED_RUNNERS" ]; then
   # Print warning messages on stderr
-  error "These self-hosted labels are not supported and will be removed from actionlint configuration"
-  error "$UNSUPPORTED_RUNNERS"
-  error
+  warn "These self-hosted labels are not supported and will be removed from actionlint configuration"
+  warn "$UNSUPPORTED_RUNNERS"
+  warn
 fi
 VALID_RUNNERS="$(comm -12 /tmp/supported_runner_labels /tmp/custom_runner_labels)"
 if [ -z "$VALID_RUNNERS" ]; then
-  error "No valid self-hosted labels found in your actionlint configuration"
+  warn "No valid self-hosted labels found in your actionlint configuration"
   # error "Fallback to supported self-hosted labels provided by infra team"
-  error
+  warn
   VALID_RUNNERS=$(cat /tmp/supported_runner_labels)
 fi
 valid_runners="$VALID_RUNNERS" yq '.self-hosted-runner.labels = env(valid_runners)' "$CUSTOM_CONFIG_FILE"
